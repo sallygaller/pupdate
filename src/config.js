f139530aa07225d855f9da5efe932018cbda643e
unknown <sallyannegaller@gmail.com>
2021-03-10 16:53:54 -0800

    Changed API endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const API_TOKEN = process.env.REACT_APP_GOOGLE_API_KEY;
 const TOKEN_KEY = "test-auth-token";
-const API_ENDPOINT =
-  process.env.REACT_APP_BASE_URL || `http://localhost:8000/api`;
+// const API_ENDPOINT =
+//   process.env.REACT_APP_BASE_URL || `http://localhost:8000/api`;
+const API_ENDPOINT = process.env.REACT_APP_BASE_URL;
 const S3_ID = process.env.REACT_APP_S3_ID;
 const S3_KEY = process.env.REACT_APP_S3_KEY;
 
